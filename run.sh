75f2cd65147a797731039faf9e9b23152bb3db2b
leogtzr <leogutierrezramirez@gmail.com>
2019-04-28 12:28:55 -0600

    remove debug sentence from runner script

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #: author: Leo Gtz <leogutierrezramirez@gmail.com>
 
-set -x
+# set -x
 readonly work_dir=$(dirname "$(readlink --canonicalize-existing "${0}")")
 readonly bin_program="${work_dir}/web"
 readonly error_program_not_found=80
 readonly error_building_program=81
 
 if [[ "${1}" == "b" ]]; then
+    echo -n "building binary artifact ... "
     go clean
     go build -o "${work_dir}/web" || exit ${error_building_program}
 fi
@@ -17,9 +18,12 @@ if [[ ! -f "${bin_program}" ]]; then
     exit ${error_program_not_found}
 fi
 
+echo "done"
+
 export BITACORA_USER=leo
 export BITACORA_PASSWORD=lein23
 
+echo "running program ..."
 "${bin_program}"
 
 exit
